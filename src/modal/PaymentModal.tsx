c51a7aaa6aae057d03d13e7285ee69ea41b4c7f7
alexconstantin <alex.constantin92@yahoo.com>
2022-11-22 00:35:20 +0200

    initial commit

--- src/modal/PaymentModal.tsx
+++ src/modal/PaymentModal.tsx
@@ -8,6 +8,9 @@ import {createSignal} from "solid-js";
 interface PaymentModalProps extends GenericModalProps {
 }
 
+const productType = 'Pro Annual';
+const subscriptionTotal = '$276.00 / year';
+
 const PaymentModal = (props: PaymentModalProps) => {
   const [btnDisabled, setBtnDisabled] = createSignal(true);
 
@@ -17,7 +20,6 @@ const PaymentModal = (props: PaymentModalProps) => {
         <span class={styles.topHeader}>Try Pro Plan for free</span>
         <span class={styles.topSubheader}>30-day free trial, cancel at any time</span>
         <span class={styles.topSubheader}>We'll remind you before your trial ends</span>
-
         <PaymentType/>
         <PaymentInformation/>
         <div class={styles.btnWrapper}>
@@ -28,8 +30,48 @@ const PaymentModal = (props: PaymentModalProps) => {
             href={'https://www.musthavemenus.com'}>Privacy Policy</a>, and agree to the recurring charges for your subscription plan until you cancel.</span>
         </div>
       </div>
+
       <div class={styles.right}>
-        right
+        <span class={styles.topHeader}>Subscription Details</span>
+
+        <div class={styles.paymentDetails}>
+          <div class={styles.paymentDetailsHeader}>
+            <span>Subscription</span>
+          </div>
+          <div class={styles.paymentDetailsEntry}>
+            <div>{productType}</div>
+            <div>{subscriptionTotal}</div>
+          </div>
+        </div>
+
+        <div class={`${styles.paymentDetails} ${styles.topBorder}`}>
+          <div class={styles.paymentDetailsHeader}>
+            <span>Add-on</span>
+            <span>Qty</span>
+            <span>&nbsp;</span>
+          </div>
+          <div class={styles.paymentDetailsEntry}>
+            <div>Users</div>
+            <div>2</div>
+            <div>$240.00 / year</div>
+          </div>
+          <div class={styles.paymentDetailsEntry}>
+            <div>Locations</div>
+            <div>1</div>
+            <div>$120.00 / year</div>
+          </div>
+        </div>
+
+        <div class={`${styles.paymentDetails} ${styles.topBorder}`}>
+          <div class={styles.paymentDetailsEntry}>
+            <div>Subtotal</div>
+            <div>$636.00 USD</div>
+          </div>
+          <div class={styles.paymentDetailsEntry}>
+            <div>Tax</div>
+            <div>$0.00 USD</div>
+          </div>
+        </div>
       </div>
     </div>
   }/>
