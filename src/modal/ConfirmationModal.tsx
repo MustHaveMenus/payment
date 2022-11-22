ff1406a97101aa102616a4fbff1701fdc9c62ada
alexconstantin <alex.constantin92@yahoo.com>
2022-11-22 20:09:10 +0200

    add confirmation page

--- src/modal/ConfirmationModal.tsx
+++ src/modal/ConfirmationModal.tsx
@@ -1,4 +1,5 @@
 import styles from './ConfirmationModal.module.scss';
+import party from '../assets/party.webp';
 import Modal, {GenericModalProps} from "../comp/Modal";
 import Button from "../comp/Button";
 
@@ -6,14 +7,14 @@ interface ConfirmationModalProps extends GenericModalProps {
 }
 
 const ConfirmationModal = (props: ConfirmationModalProps) => {
-  return <Modal onClose={props.onClose} onBack={props.onBack} content={
+  return <Modal onClose={props.onClose} content={
     <div class={styles.wrapper}>
-      <span>img</span>
+      <img src={party} alt={''}/>
       <span class={styles.textMd}>You're all set!</span>
       <span class={styles.textSm}>We sent you a confirmation.</span>
       <span class={styles.textLg}>We hope you enjoy your free trial!</span>
 
-      <Button class={styles.btn} label={'Done'}/>
+      <Button class={styles.btn} label={'Done'} onClick={props.onClose}/>
     </div>
   }/>
 }
