5cdc6461c1dd245e97b1565678434dc692b55d94
alexconstantin <alex.constantin92@yahoo.com>
2022-11-24 12:19:26 +0200

    changes

--- src/modal/ConfirmationModal.tsx
+++ src/modal/ConfirmationModal.tsx
@@ -3,20 +3,29 @@ import party from '../assets/party.webp';
 import Modal, {GenericModalProps} from "../comp/Modal";
 import Button from "../comp/Button";
 import openState from "../state/open";
+import footerStyles from "../comp/ModalFooter.module.scss";
+import mobileState from "../state/mobile";
 
 interface ConfirmationModalProps extends GenericModalProps {
 }
 
 const ConfirmationModal = (props: ConfirmationModalProps) => {
   const {closeModal} = openState;
-  return <Modal content={
+  const {mobile} = mobileState;
+
+  const doneBtn = () => <Button class={styles.btn} label={'Done'} onClick={closeModal}/>;
+
+  const mobileFooter = () => mobile() ? <div class={footerStyles.borderedFooter}>{doneBtn()}</div> : undefined;
+  const desktopFooter = () => !mobile() ? doneBtn() : <></>
+
+  return <Modal footer={mobileFooter()} content={
     <div class={styles.wrapper}>
       <img src={party} alt={''}/>
       <span class={styles.textMd}>You're all set!</span>
       <span class={styles.textSm}>We sent you a confirmation.</span>
       <span class={styles.textLg}>We hope you enjoy your free trial!</span>
 
-      <Button class={styles.btn} label={'Done'} onClick={closeModal}/>
+      {desktopFooter()}
     </div>
   }/>
 }
