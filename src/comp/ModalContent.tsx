9ad74526ae43f3e690d9f9c635446510025c7933
alexconstantin <alex.constantin92@yahoo.com>
2022-11-24 09:49:11 +0200

    changes

--- src/comp/ModalContent.tsx
+++ src/comp/ModalContent.tsx
@@ -1,5 +1,6 @@
 import styles from './ModalContent.module.scss';
 import {JSXElement} from "solid-js";
+import mobileState from "../state/mobile";
 
 interface ModalContentProps {
   children?: JSXElement,
@@ -8,10 +9,12 @@ interface ModalContentProps {
 }
 
 const ModalContent = (props: ModalContentProps) => {
+  const {mobile} = mobileState;
   return <div classList={{
     [styles.modalContent]: true,
     [styles.withFooter]: props.withFooter,
-    [styles.withHeader]: props.withHeader
+    [styles.withHeader]: props.withHeader,
+    [styles.mobile]: mobile(),
   }}>{props.children}</div>
 }
 export default ModalContent;
