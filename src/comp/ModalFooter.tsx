01ea60990fe86daf0330625ab97b32e6b8858538
alexconstantin <alex.constantin92@yahoo.com>
2022-11-24 10:27:22 +0200

    changes

--- src/comp/ModalFooter.tsx
+++ src/comp/ModalFooter.tsx
@@ -1,11 +1,16 @@
 import styles from './ModalFooter.module.scss';
 import {JSXElement} from "solid-js";
+import mobileState from "../state/mobile";
 
 interface ModalFooterProps {
   children?: JSXElement,
 }
 
 const ModalFooter = (props: ModalFooterProps) => {
-  return <div class={`${styles.modalFooter}`}>{props.children}</div>
+  const {mobile} = mobileState;
+  return <div classList={{
+    [styles.modalFooter]: true,
+    [styles.mobile]: mobile()
+  }}>{props.children}</div>
 }
 export default ModalFooter;
