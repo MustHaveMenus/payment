9d08fa56169554fac43a3eca86c0acceaedf0e15
alexconstantin <alex.constantin92@yahoo.com>
2022-11-24 11:26:41 +0200

    changes

--- src/modal/TeamModal.tsx
+++ src/modal/TeamModal.tsx
@@ -4,7 +4,7 @@ import headerStyles from '../comp/ModalHeader.module.scss';
 import generalStyles from '../style/general.module.scss';
 import Modal, {GenericModalProps} from "../comp/Modal";
 import Button from "../comp/Button";
-import {createEffect, For, onMount, Show} from "solid-js";
+import {createEffect, For, Show} from "solid-js";
 import TrashIcon from "../comp/svg/TrashIcon";
 import Input from "../comp/Input";
 import Select from "../comp/Select";
@@ -14,11 +14,13 @@ import {User} from "../type/types";
 import teamState from "../state/team";
 import locationState from "../state/location";
 import {LocationDto} from "../generated/client";
+import mobileState from "../state/mobile";
 
 interface TeamModalProps extends GenericModalProps {
 }
 
 const TeamModal = (props: TeamModalProps) => {
+  const {mobile} = mobileState;
   const {locations, addLocations} = locationState;
   const {team, addUser, deleteUser, addLocation, deleteLocation, updateLocation, updateEmail} = teamState;
 
@@ -53,67 +55,71 @@ const TeamModal = (props: TeamModalProps) => {
   }
 
   return <Modal onBack={props.onBack}
-    header={
-      <div class={headerStyles.wrapper}>
-        <span class={headerStyles.header}>Add Your Team</span>
-        <span class={headerStyles.subheader}>Try Teams for free for 30 days. Only ${teamPricePerMonth}/month per user after that.</span>
-        <span class={headerStyles.subheader}>New users will get an email invite.</span>
-      </div>
-    }
-    content={
-      <div class={styles.wrapper}>
-        <div class={styles.form}>
-          <For each={team[USERS]}>{(user, i) =>
-            <div class={styles.entry}>
-              <div>
-                <div class={styles.formHeader}>
-                  <span>New User Info</span>
-                  <span class={`${i() === 0 ? generalStyles.invisible : ''}`} onClick={[deleteUser, user]}>
+                header={
+                  <div class={headerStyles.wrapper}>
+                    <span class={headerStyles.header}>Add Your Team</span>
+                    <span class={headerStyles.subheader}>Try Teams for free for 30 days. Only ${teamPricePerMonth}/month per user after that.</span>
+                    <span class={headerStyles.subheader}>New users will get an email invite.</span>
+                  </div>
+                }
+                content={
+                  <div classList={{[styles.wrapper]: true, [styles.mobile]: mobile()}}>
+                    <div class={styles.form}>
+                      <For each={team[USERS]}>{(user, i) =>
+                        <div class={styles.entry}>
+                          <div>
+                            <div class={styles.formHeader}>
+                              <span>New User Info</span>
+                              <span class={`${i() === 0 ? generalStyles.invisible : ''}`} onClick={[deleteUser, user]}>
                                 <TrashIcon/>
                               </span>
-                </div>
+                            </div>
 
-                <div class={styles.formContent}>
-                  <Input type={'text'} value={user.email} placeholder={'Email address'}
-                         onKeyUp={(e) => updateEmail(user, (e.target as HTMLInputElement)?.value ?? '')}/>
-                </div>
-              </div>
-              <div>
-                <div class={styles.formHeader}>
-                  <span>Location Assignment</span>
-                  <span class={generalStyles.invisible}>
+                            <div class={styles.formContent}>
+                              <Input type={'text'} value={user.email} placeholder={'Email address'}
+                                     onKeyUp={(e) => updateEmail(user, (e.target as HTMLInputElement)?.value ?? '')}/>
+                            </div>
+                          </div>
+                          <div>
+                            <div class={styles.formHeader}>
+                              <span>Location Assignment</span>
+                              <span class={generalStyles.invisible}>
                                 <TrashIcon/>
                               </span>
-                </div>
-                <div class={styles.formContent}>
-                  <For each={user.locations}>{(loc, i) =>
-                    <div class={styles.locationEntry}>
-                      <Select values={locations[LOCATIONS]} disabledValues={user.locations} value={loc}
-                              onChange={(newLoc) => updateLocation(user, loc, newLoc as LocationDto)}/>
-                      <span class={`${i() === 0 ? generalStyles.invisible : ''}`} onClick={() => deleteLocation(user, loc)}>
-                                    <TrashIcon/>
-                                  </span>
-                    </div>
-                  }</For>
-                  <Show when={remainingLocations(user).length} keyed>
-                    <div class={styles.addLocation}>
-                      <span onClick={() => addLocation(user, newLocation(user))}>+ Location Assignment</span>
-                    </div>
-                  </Show>
-                </div>
-              </div>
-            </div>
-          }</For>
+                            </div>
+                            <div class={styles.formContent}>
+                              <For each={user.locations}>{(loc, i) =>
+                                <div class={styles.locationEntry}>
+                                  <Select values={locations[LOCATIONS]} disabledValues={user.locations} value={loc}
+                                          onChange={(newLoc) => updateLocation(user, loc, newLoc as LocationDto)}/>
+                                  <Show when={!mobile() || i() > 0} keyed>
+                                    <span class={`${i() === 0 ? generalStyles.invisible : ''}`} onClick={() => deleteLocation(user, loc)}>
+                                      <TrashIcon/>
+                                    </span>
+                                  </Show>
+                                </div>
+                              }</For>
+                              <Show when={remainingLocations(user).length} keyed>
+                                <div class={styles.addLocation}>
+                                  <span onClick={() => addLocation(user, newLocation(user))}>+ Location Assignment</span>
+                                </div>
+                              </Show>
+                            </div>
+                          </div>
+                        </div>
+                      }</For>
 
-          <Button class={styles.addUser} label={'+ Add Another User'} onClick={addNewUser}/>
-        </div>
-      </div>
-    }
-    footer={
-      <div class={footerStyles.teamFooter}>
-        <Button onClick={validateAndProceed} label={'Next'}></Button>
-        <span onClick={props.onNext}>Skip this step {'>'}</span>
-      </div>
-    }/>
+                      <div class={styles.addUserWrapper}>
+                        <Button label={'+ Add Another User'} onClick={addNewUser}/>
+                      </div>
+                    </div>
+                  </div>
+                }
+                footer={
+                  <div class={footerStyles.teamFooter}>
+                    <Button onClick={validateAndProceed} label={'Next'}></Button>
+                    <span onClick={props.onNext}>Skip this step {'>'}</span>
+                  </div>
+                }/>
 }
 export default TeamModal;
