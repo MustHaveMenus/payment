338fbd87e1c286332391379937f168e52e2f5827
alexconstantin <alex.constantin92@yahoo.com>
2022-11-30 14:33:58 +0200

    upgrade

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
-import {env} from "../env/env";
 import Storage from "../util/Storage";
 import {Configuration} from "../generated/client";
+import {env} from "../env/env";
 
 export const ops = (loginToken: string = "") => {
   if (!Storage.getToken() && !loginToken) return {};
@@ -13,4 +13,16 @@ export const ops = (loginToken: string = "") => {
   };
 };
 
-export const apiConfig = new Configuration({basePath: env.apiPath});
+const getApiPath = () => {
+  const host = window.location.host;
+  if (host === 'test.mhmfun.com') {
+    return 'https://test.mhmfun.com/account-test';
+  } else if (host === 'staging.mhmfun.com') {
+    return 'https://www.musthavemenus.com/account-stage';
+  } else if (host === 'www.musthavemenus.com') {
+    return 'https://www.musthavemenus.com/account';
+  }
+  return env.apiPath;
+}
+
+export const apiConfig = new Configuration({basePath: getApiPath()});
