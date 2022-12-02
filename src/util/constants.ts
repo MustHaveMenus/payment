9a34882049dc87f2201c0ff5c1675f568429043f
alexconstantin <alex.constantin92@yahoo.com>
2022-12-02 15:04:00 +0200

    changes

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,3 +1,16 @@
+import {LocationDto} from "../generated/client";
+import {User} from "../type/types";
+
 export const USERS = 'users';
 export const LOCATIONS = 'locations';
 export const EMAIL = 'email';
+
+export const DEFAULT_LOCATION = {
+  country: 'United States of America',
+  state: 'Alabama'
+} as LocationDto;
+
+export const DEFAULT_USER = {
+  email: '',
+  locations: []
+} as User
