a6a29f5ae1c05ed63ec946aa6213d60424b90051
alexconstantin <alex.constantin92@yahoo.com>
2022-12-05 21:30:03 +0200

    Terms of Use and Privacy Policy are taking to home page

--- src/comp/Agreement.tsx
+++ src/comp/Agreement.tsx
@@ -9,8 +9,8 @@ const Agreement = (props: AgreementProps) => {
   const {mobile} = mobileState;
 
   return <div classList={{[styles.agreement]: true, [styles.mobile]: mobile()}}>
-          <span>By continuing, you agree to our <a href={'https://www.musthavemenus.com'} target={'_blank'}>Terms of Use</a>, confirm you have read our <a
-            href={'https://www.musthavemenus.com'} target={'_blank'}>Privacy Policy</a>, and agree to the recurring charges for your subscription plan until you cancel.</span>
+          <span>By continuing, you agree to our <a href={'/menu/usagePolicy/view.do'} target={'_blank'}>Terms of Use</a>, confirm you have read our <a
+            href={'/menu/privacyPolicy/view.do'} target={'_blank'}>Privacy Policy</a>, and agree to the recurring charges for your subscription plan until you cancel.</span>
   </div>
 }
 export default Agreement;
